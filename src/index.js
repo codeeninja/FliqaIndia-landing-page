cdfd62b77d8ee09a30e4fc9cd0c21441d9a5e519
codeeninja <102271798+codeeninja@users.noreply.github.com>
2023-06-21 09:54:25 +0530

    Add production build files

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const root = ReactDOM.render(
+  <HashRouter>
     <App />
-  </React.StrictMode>
+  </HashRouter>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
